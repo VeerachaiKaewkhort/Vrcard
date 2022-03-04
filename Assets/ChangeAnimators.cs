d244cf5c28ac67ba4976d0a7a913cd82b9fa9dd2
VeerachaiKaewkhort <95077586+VeerachaiKaewkhort@users.noreply.github.com>
2022-03-04 18:52:27 +0700

    Vrcard

---
+++ Assets/ChangeAnimators.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void getDif(){
+        this.GetComponent<Animator>().Play("Defeated");
+    }
+    public void getHip(){
+        this.GetComponent<Animator>().Play("Hip Hop Dancing");
+    }
+}
